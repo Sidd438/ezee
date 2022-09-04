60d5fc5e8f202fbba03a719322d77d7321505967
Siddharth <f20210438@pilani.bits-pilani.ac.in>
2022-09-04 23:49:20 +0530

    trial

--- src/server.rs
+++ src/server.rs
@@ -1,15 +1,33 @@
-    pub struct Server {
-        port: String,
-    }
-    impl Server {
-        pub fn run(port:String) -> Self {
-            println!("cargo run");
-            return Server {
-                port
-            };
-        }
+use std::net::TcpListener;
+use std::io::Read;
 
-        pub fn start(self){
-            println!("Server started on port {}", self.port);
+pub struct Server {
+    addr: String,
+}
+impl Server {
+    pub fn run(port:String) -> Self {
+        println!("cargo run");
+        return Server {
+            addr:port
+        };
+    }
+    pub fn start(self){
+        println!("Server started on address {}", self.addr);
+
+        let listner = TcpListener::bind(&self.addr).unwrap();
+        loop {
+            match listner.accept(){
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Recieved a request: {}", String::from_utf8_lossy(&buffer))
+                        }
+                        Err(e) => println!("Failed to Establish connection: {}", e) 
+                    }
+                }
+                Err(e) => println!("Failed to Establish connection: {}", e)
+            }
         }
     }
+}
